222d06c2c27ba206617ef8850e962d62072a5a64
ayman <ayman.mohamed141163@gmail.com>
2024-09-01 01:54:59 +0300

    'save changes'

--- Q&A-chatbot-stremlit.py
+++ Q&A-chatbot-stremlit.py
@@ -11,18 +11,22 @@ GeminiChat = ChatGoogleGenerativeAI(model='gemini-pro', google_api_key=google_ap
 # create prompt 
 initial_prompt = """
 You are Q&A chatbot make conversations in different fileds.
-When get question from user make your response concise.
+When get question from user make your response concise and oraginzed.
 If you don't know question answer say i don't know.
 """
 # Streamlit UI
 st.title('Conversation Q&A Chatbot')
 
+# Button to clear all messages
+if st.button('Clear Messages'):
+    # Clear session state for messages and history
+    st.session_state.messages = []
+    # Rerun the app to refresh the state
+    st.experimental_rerun()  
+
 # Initialize session state for messages and history
 if 'messages' not in st.session_state:
-    st.session_state.messages = []
-if 'history' not in st.session_state:
-    st.session_state.history = ChatMessageHistory() 
-    st.session_state.history.add_message(initial_prompt)  
+    st.session_state.messages = [] 
 
 
 # Display previous messages from the session state
@@ -39,25 +43,25 @@ if prompt:
         st.write(prompt)
     
     # Add user message to session history and update messages
-    st.session_state.history.add_user_message(prompt)
     st.session_state.messages.append({'role': 'user', 'content': prompt})
 
     # Generate response from model 
-    response_chunks = GeminiChat.stream(st.session_state.history.messages)
+    response_chunks = GeminiChat.stream(
+        [msg['content']  for msg in st.session_state.messages]
+        )
     
     # Collect response content for appending to history
     response_content = []
     
     # Display assistant's response in real-time
-    with st.chat_message('assistant', avatar='assistant'):
+    with st.chat_message('assistant'):
         for chunk in response_chunks:
-            st.write(chunk.content)  
+            st.write(chunk.content, end='', flush=True)  
             response_content.append(chunk.content)
 
     # Combine all response chunks
     complete_response = ' '.join(response_content)
     #  Add assistant message to history and session state
-    st.session_state.history.add_ai_message(complete_response)
     st.session_state.messages.append({'role': 'assistant', 'content': complete_response})
 
     
